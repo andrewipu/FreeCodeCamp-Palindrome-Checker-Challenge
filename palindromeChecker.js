b2656fe1da23deca06c06165fc03ed461d7720c3
Andrew Ipu <ipuandrew@gmail.com>
2024-02-22 21:03:09 +0300

    Fundamental features implemented

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -1,5 +1,18 @@
 function palindrome(str) {
-    return true;
+//?standardize the input
+//remove non-alphanumeric characters
+let inputStr = str.replace(/\W/g, '');
+
+//make sure it's lowercase
+inputStr = inputStr.toLowerCase();
+
+//Reverse the input string
+let inputReverse = inputStr.split('').reverse().join("");
+console.log(inputReverse);
+
+//Compare the original and reversed strings
+return console.log(inputStr === inputReverse);
+    
   }
   
-  palindrome("eye");
+palindrome("eye");
