d90b9ab3a152bd9d1645b74695e606af5aad690d
Andrew Ipu <ipuandrew@gmail.com>
2024-02-22 21:55:56 +0300

    I need to filter out underscores and the like

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -2,17 +2,30 @@ function palindrome(str) {
 //?standardize the input
 //remove non-alphanumeric characters
 let inputStr = str.replace(/\W/g, '');
+console.log(inputStr);
 
 //make sure it's lowercase
 inputStr = inputStr.toLowerCase();
 
 //Reverse the input string
-let inputReverse = inputStr.split('').reverse().join("");
+let inputReverse = inputStr.split('').reverse().join('');
 console.log(inputReverse);
 
 //Compare the original and reversed strings
-return console.log(inputStr === inputReverse);
+let check = "";
+if (inputStr === inputReverse)
+{
+    check = true;
+} else {
+    check = false;
+}
+console.log(check);
+return check;
+
+
+//return console.log(inputStr === inputReverse);
+
     
   }
   
-palindrome("eye");
+palindrome("not a palindrome");
